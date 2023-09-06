d242ac992fa6a2ca270c6afdfeca1e58234def15
186064 <90035571+186064@users.noreply.github.com>
2023-09-06 12:27:45 +0530

    Update execute_sql_to_gcs_dag.py

--- dags/execute_sql_to_gcs_dag.py
+++ dags/execute_sql_to_gcs_dag.py
@@ -1,14 +1,24 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
-from airflow.operators.email import EmailOperator
+from airflow.utils.dates import days_ago
+from google.cloud import bigquery
 import subprocess
 
  
 
 def execute_sql_script():
-    # Your SQL execution logic here
-    pass
+    gcs_blob_path = "gs://taskbucket1/my_folder/Customerscript.sql"
+    gsutil_cmd = ["gsutil", "cat", gcs_blob_path]
+
+    try:
+        sql_script = subprocess.check_output(gsutil_cmd, universal_newlines=True, stderr=subprocess.STDOUT)
+        client = bigquery.Client()
+        query_job = client.query(sql_script)
+        for row in query_job:
+            print(row)
+    except subprocess.CalledProcessError as e:
+        print("Error executing gsutil command:", e.output)
 
  
 
@@ -25,7 +35,7 @@ default_args = {
 with DAG(
     "execute_sql_to_gcs_dag",
     default_args=default_args,
-    schedule=None,
+    schedule_interval=None, 
     catchup=False,
 ) as dag:
 
@@ -36,8 +46,6 @@ with DAG(
         python_callable=execute_sql_script,
     )
 
-    # Define other tasks (e.g., send_email_task) as needed
-
  
 
-    # Set task dependencies as needed
+    execute_sql_task
