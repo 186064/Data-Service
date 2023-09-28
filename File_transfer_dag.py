d2f93037d9bdd3f69c46e913ba9195b38a3fa647
186064 <186064@ust-global.com>
2023-09-28 11:24:35 +0530

    genrated dag

---
+++ File_transfer_dag.py
@@ -0,0 +1,39 @@
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+BQ_PROJECT = "dataservices-271014"
+BQ_DATASET = "customer_task"
+BQ_TABLE = "sales_data"
+GCS_BUCKET = "taskbucket1"
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 8, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    dag_id='File_transfer',
+    default_args=default_args,
+    description='Transfering file from gcs to bigquery',
+    schedule=nan,
+    catchup=False,
+)
+
+transfer_file_to_bq = GCSToBigQueryOperator(
+    task_id='File_transfer',
+    bucket='taskbucket1',
+    source_objects='my_folder/mydata.csv',
+    destination_project_dataset_table='dataservices-271014.customer_task.sales_data',
+    autodetect=True,
+    source_format='CSV',
+    skip_leading_rows=1,
+    write_disposition='WRITE_TRUNCATE',
+    dag=dag,
+)
